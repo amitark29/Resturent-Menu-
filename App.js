b474e935a574a35c5046da84f609592756eab698
Amit Kumar <113199810+amitark29@users.noreply.github.com>
2023-04-16 15:09:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Resturent from "./component/Basics/Resturent";
-import Todo from "./component/TodoReact/todo";
 
 const App = () => {
   return (
     <>
     <Resturent/>
-    {/* <Todo/> */}
     </>
   
     
